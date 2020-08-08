89102887406c9582552a3dd5d1329783c5f90424
imjchiang <jchiang296@gmail.com>
2020-08-07 17:02:13 -0700

    [CSS] add some styling [EJS] implement styling [JS] change number of pokemons shown

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static("public"));
 
 // GET - main index of site
 app.get('/', function(req, res) 
 {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=964>';
   // Use request to call the API
   axios.get(pokemonUrl)
   .then(response => 
   {
     let pokemon = response.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    res.render('index', { pokemon: pokemon.slice(0, 964) });
   });
 });
 
