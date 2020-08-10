4cd37bc746f4955e824b36c201080d9e7583a60b
imjchiang <jchiang296@gmail.com>
2020-08-09 23:18:38 -0700

    [NPM] add method override [JS] implement method override, render favs directory, find or create pokemon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,29 +5,38 @@ var db = require("../models");
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) 
 {
-  // Make sure to require your models in the files where they will be used.
-  db.pokemon.create(
-  {
-    name: 'Pikachu'
-  })
-  .then(function(poke) 
-  {
-    console.log('Created: ', poke.name)
-  });
-
   db.pokemon.findAll().then(function(poke) 
   {
     console.log('Found: ', poke.name);
   });
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  res.render("favs", {pokemon: db.pokemon.findAll()});
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) 
 {
+  // Make sure to require your models in the files where they will be used.
+  // db.pokemon.create(
+  //   {
+  //     name: req.params.name
+  //   })
+  //   .then(function(poke) 
+  //   {
+  //     console.log('Created: ', poke.name)
+  //   });
+
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate(
+    {
+      where:
+      {
+        name: req.body.name
+      }
+    }
+  )
+  res.redirect("/pokemon");
+  //res.send(req.body);
 });
 
 module.exports = router;
